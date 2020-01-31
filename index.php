33594d93178ab4fa0ec734b568615e58ef94b955
tazbin <md.tazbinur.rahaman@gmail.com>
2020-01-31 11:35:21 +0600

    project initialized

---
+++ index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Pursuit Of Impact</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <!-- my code goes here -->
+
+    <!-- home section -->
+    <div class="home-section" style="background-image: url('img/head-cover.png');">
+      <div class="container">
+
+        <div class="desktop">
+          <div class="row">
+            <div class="col-md-5">
+              <div class="quote">
+                "Solving the world's most pressing challenge is not the right thing to do, it is also a good business"
+              </div>
+              <div class="author text-right">
+                <hr>
+                <b>Ahmed Ashkar</b> <br>
+                CEO & Founder of Hult Prize
+              </div>
+            </div>
+            <div class="col-md-7 text-left">
+              <img src="img/book.png" class="book" alt="">
+            </div>
+          </div>
+        </div>
+
+        <div class="row mobile">
+          <div class="col-md-12 text-center">
+            <img src="img/book.png" class="m-book" alt="">
+          </div>
+          <div class="col-md-12">
+            <div class="m-quote">
+              "Solving the world's most pressing challenge is not the right thing to do, it is also a good business"
+            </div>
+            <div class="m-author text-right">
+              <hr>
+              <b>Ahmed Ashkar</b> <br>
+              CEO & Founder of Hult Prize
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <!-- home section -->
+
+    <!-- ashkar introduction -->
+    <div class="container intro">
+      <div class="row">
+        <div class="col-lg-4 col-md-12 text-center">
+          <img src="img/ashkar.png" class="intro-img" alt="">
+        </div>
+        <div class="col-lg-8 col-md-12 text-left" style="margin-top: 50px;">
+          <span class="h1-black">AHMAD</span> <br>
+          <span class="h1-orange">ASHKAR</span> <br>
+          <span class="h3-black">SOCIAL</span> <span class="h3-orange">VENTURE EXECUTIVE</span>
+          <p class="intro-text">
+            Chief Executive Officer with more than a decade of top-level management experience in both non-profit and for-profit sector. Dynamic experience in creating and growing companies and recognized as a 2018 GQ Entrepreneur of the Year, Esquire Magazine Entrepreneur of the Year 2017 and Arabian Business CEO of the Year 2016. Most recently, in 2018, Fast Company recognized the Hult Prize <br>
+            Foundation as one of the world's most innovative. Multiple successful deployments in education, real estate, finance, advisory, hospitality, wellness and food and beverage.
+          </p>
+          <hr class="intro-hr">
+          <a href="#"> <img src="img/linkedin.png" alt="linkedin" class="linkedin"> <span class="linkedin-text"> aashkar </span> </a>
+        </div>
+      </div>
+    </div>
+    <!-- ashkar introduction -->
+
+    <!-- showcase section -->
+    <div class="text-center">
+      <div class="container-showcase mx-auto">
+        <div class="row showcase">
+          <div class="col-lg-4 col-md-6 text-center showcase-col">
+            <img src="img/awards.png" alt="">
+          </div>
+          <div class="col-lg-4 col-md-6 text-center showcase-col">
+            <img src="img/gulf_business.png" alt="">
+          </div>
+          <div class="col-lg-4 col-md-6 text-center showcase-col">
+            <img src="img/fast_company.png" alt="">
+          </div>
+          <div class="col-lg-4 col-md-6 text-center showcase-col">
+            <img src="img/gq.png" alt="">
+          </div>
+          <div class="col-lg-4 col-md-6 text-center showcase-col">
+            <img src="img/business.png" alt="">
+          </div>
+          <div class="col-lg-4 col-md-6 text-center showcase-col">
+            <img src="img/esquire.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- showcase section -->
+
+    <!-- experience section -->
+    <div class="container-showcase mx-auto">
+      <div class="ex-title text-center">
+        EXPERIENCE
+      </div>
+
+      <div class="row container ex-row">
+        <div class="col-lg-3 col-md-12">
+          <img src="img/hult_prize.png" class="experience-img" alt="">
+        </div>
+        <div class="col-lg-9 col-md-12 experience-text">
+          <h2 class="ex-company"> <img src="img/point.png" class="ex-point" alt=""> CEO & Founder </h2>
+          <h4 class="ex-position"> Hult Prize Foundation </h4>
+          <p class="ex-details">
+            The Hult Prize is a start-up accelerator for social enterprise named by President Bill Clinton and TIME Magazine as one of the top five ideas changing the world. The initiatives that make up the work of the Hult Prize Foundation are all dedicated to launching the next wave of social entrepreneurs through building custom tailored start-ups which are impact centered and profit minded.  In partnership with President Clinton and the United Nations (formerly the Clinton Global Initiative) the Hult Prize has created the world’s largest student movement for social impact and the world’s largest crowdsourcing platform, dedicated to solving the most pressing social challenges on the planet.
+          </p>
+        </div>
+      </div>
+
+      <div class="row container ex-row">
+        <div class="col-lg-3 col-md-12">
+          <img src="img/falafel.png" class="experience-img" alt="">
+        </div>
+        <div class="col-lg-9 col-md-12 experience-text">
+          <h2 class="ex-company"> <img src="img/point.png" class="ex-point" alt=""> Founder </h2>
+          <h4 class="ex-position"> falafel Inc. </h4>
+          <p class="ex-details">
+            Falafel Inc is the #1 New Restaurant in the DC, Metro and among the top in America as rated by Yelp and Buzzfeed. It is the world’s first falafel quick-service food social enterprise. The company serves up authentic falafel, hummus, bowls and sides. Every meal you buy helps feed refugees around the world. “Food For Good” and "Eat with Dignity" is the ethos be
+            hind our brand and movement. ​Legendary recipes passed down through generations, fresh and locally sourced ingredients and a social mission to provide meals and jobs to refugees through the sale of Palestinian inspired street food. The goal is to build a worldwide community of 100 stores, and in doing so we will be able to feed 1,000,000 refugees every year. Now open or opening soon in Boston, Washington, DC, Amman, Dubai, Mexico City, San Francisco, Denver and Jerusalem.
+          </p>
+        </div>
+      </div>
+
+      <div class="row container ex-row">
+        <div class="col-lg-3 col-md-12">
+          <img src="img/undp.png" class="experience-img" alt="">
+        </div>
+        <div class="col-lg-9 col-md-12 experience-text">
+          <h2 class="ex-company"> <img src="img/point.png" class="ex-point" alt=""> Advisory Board </h2>
+          <h4 class="ex-position"> UNDP </h4>
+          <p class="ex-details">
+            Serve on the eight person Advisory Board for the United Nations Development Program (UNDP) Innovation Facility. The UNDP works in more than 170 countries and territories, helping to achieve the eradication of poverty, and the reduction of inequalities and exclusion. They help countries to develop policies, leadership skills, partnering abilities, institutional capabilities and build resilience in order to sustain development results.
+          </p>
+        </div>
+      </div>
+
+    </div>
+    <!-- experience section -->
+
+    <!-- impact backgroud -->
+      <div class="impact-section" style="background-image: url('img/impact-bg.png')">
+
+      </div>
+    <!-- impact backgroud -->
+
+    <!-- my code ends here -->
+  </body>
+  <script src="js/bootstrap.min.js" charset="utf-8"></script>
+  <script src="js/bootstrap.bundle.min.js" charset="utf-8"></script>
+</html>
