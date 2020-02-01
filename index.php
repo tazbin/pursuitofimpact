9cfbc083007b00c1b10497f9bbc9442023a53b23
tazbin <md.tazbinur.rahaman@gmail.com>
2020-02-01 20:00:50 +0600

    set for desktop & mobile

--- index.php
+++ index.php
@@ -26,14 +26,14 @@
               </div>
             </div>
             <div class="col-md-7 text-left">
-              <img src="img/book.png" class="book" alt="">
+              <img src="img/book2.png" class="book" alt="">
             </div>
           </div>
         </div>
 
         <div class="row mobile">
           <div class="col-md-12 text-center">
-            <img src="img/book.png" class="m-book" alt="">
+            <img src="img/book2.png" class="m-book" alt="">
           </div>
           <div class="col-md-12">
             <div class="m-quote">
@@ -52,7 +52,7 @@
     <!-- home section -->
 
     <!-- ashkar introduction -->
-    <div class="container intro">
+    <div class="intro">
       <div class="row">
         <div class="col-lg-4 col-md-12 text-center">
           <img src="img/ashkar.png" class="intro-img" alt="">
@@ -61,7 +61,11 @@
           <span class="h1-black">AHMAD</span> <br>
           <span class="h1-orange">ASHKAR</span> <br>
           <span class="h3-black">SOCIAL</span> <span class="h3-orange">VENTURE EXECUTIVE</span>
-          <p class="intro-text">
+          <p class="intro-text desktop">
+            Chief Executive Officer with more than a decade of top-level management experience in both non-profit and for-profit sector. Dynamic experience in creating and growing companies and recognized as a 2018 GQ Entrepreneur of the Year, Esquire Magazine Entrepreneur of the Year 2017 and Arabian Business CEO of the Year 2016. Most recently, in 2018, Fast Company recognized the Hult Prize <br>
+            Foundation as one of the world's most innovative. Multiple successful deployments in education, real estate, finance, advisory, hospitality, wellness and food and beverage.
+          </p>
+          <p class="intro-text mobile" style="font-size: 30px;">
             Chief Executive Officer with more than a decade of top-level management experience in both non-profit and for-profit sector. Dynamic experience in creating and growing companies and recognized as a 2018 GQ Entrepreneur of the Year, Esquire Magazine Entrepreneur of the Year 2017 and Arabian Business CEO of the Year 2016. Most recently, in 2018, Fast Company recognized the Hult Prize <br>
             Foundation as one of the world's most innovative. Multiple successful deployments in education, real estate, finance, advisory, hospitality, wellness and food and beverage.
           </p>
@@ -105,41 +109,41 @@
         EXPERIENCE
       </div>
 
-      <div class="row container ex-row">
-        <div class="col-lg-3 col-md-12">
-          <img src="img/hult_prize.png" class="experience-img" alt="">
+      <div class="row">
+        <div class="col-lg-3 col-md-12 text-center">
+          <img src="img/hult_prize.png" class="company-logo" alt="">
         </div>
-        <div class="col-lg-9 col-md-12 experience-text">
-          <h2 class="ex-company"> <img src="img/point.png" class="ex-point" alt=""> CEO & Founder </h2>
-          <h4 class="ex-position"> Hult Prize Foundation </h4>
-          <p class="ex-details">
+        <div class="col-lg-9 col-md-12 text-left">
+          <h2 class="company-position"> <img src="img/point.png" class="position-point" alt=""> CEO & Founder </h2>
+          <h4 class="company-name"> Hult Prize Foundation </h4>
+          <p class="company-details">
             The Hult Prize is a start-up accelerator for social enterprise named by President Bill Clinton and TIME Magazine as one of the top five ideas changing the world. The initiatives that make up the work of the Hult Prize Foundation are all dedicated to launching the next wave of social entrepreneurs through building custom tailored start-ups which are impact centered and profit minded.  In partnership with President Clinton and the United Nations (formerly the Clinton Global Initiative) the Hult Prize has created the world’s largest student movement for social impact and the world’s largest crowdsourcing platform, dedicated to solving the most pressing social challenges on the planet.
           </p>
         </div>
       </div>
 
-      <div class="row container ex-row">
-        <div class="col-lg-3 col-md-12">
-          <img src="img/falafel.png" class="experience-img" alt="">
+      <div class="row">
+        <div class="col-lg-3 col-md-12 text-center">
+          <img src="img/falafel.png" class="company-logo" alt="">
         </div>
-        <div class="col-lg-9 col-md-12 experience-text">
-          <h2 class="ex-company"> <img src="img/point.png" class="ex-point" alt=""> Founder </h2>
-          <h4 class="ex-position"> falafel Inc. </h4>
-          <p class="ex-details">
+        <div class="col-lg-9 col-md-12 text-left">
+          <h2 class="company-position"> <img src="img/point.png" class="position-point" alt=""> Founder </h2>
+          <h4 class="company-name"> falafel Inc. </h4>
+          <p class="company-details">
             Falafel Inc is the #1 New Restaurant in the DC, Metro and among the top in America as rated by Yelp and Buzzfeed. It is the world’s first falafel quick-service food social enterprise. The company serves up authentic falafel, hummus, bowls and sides. Every meal you buy helps feed refugees around the world. “Food For Good” and "Eat with Dignity" is the ethos be
             hind our brand and movement. ​Legendary recipes passed down through generations, fresh and locally sourced ingredients and a social mission to provide meals and jobs to refugees through the sale of Palestinian inspired street food. The goal is to build a worldwide community of 100 stores, and in doing so we will be able to feed 1,000,000 refugees every year. Now open or opening soon in Boston, Washington, DC, Amman, Dubai, Mexico City, San Francisco, Denver and Jerusalem.
           </p>
         </div>
       </div>
 
-      <div class="row container ex-row">
-        <div class="col-lg-3 col-md-12">
-          <img src="img/undp.png" class="experience-img" alt="">
+      <div class="row">
+        <div class="col-lg-3 col-md-12 text-center">
+          <img src="img/undp.png" class="company-logo" alt="">
         </div>
-        <div class="col-lg-9 col-md-12 experience-text">
-          <h2 class="ex-company"> <img src="img/point.png" class="ex-point" alt=""> Advisory Board </h2>
-          <h4 class="ex-position"> UNDP </h4>
-          <p class="ex-details">
+        <div class="col-lg-9 col-md-12 text-left">
+          <h2 class="company-position"> <img src="img/point.png" class="position-point" alt=""> Advisory Board </h2>
+          <h4 class="company-name"> UNDP </h4>
+          <p class="company-details">
             Serve on the eight person Advisory Board for the United Nations Development Program (UNDP) Innovation Facility. The UNDP works in more than 170 countries and territories, helping to achieve the eradication of poverty, and the reduction of inequalities and exclusion. They help countries to develop policies, leadership skills, partnering abilities, institutional capabilities and build resilience in order to sustain development results.
           </p>
         </div>
@@ -149,25 +153,31 @@
     <!-- experience section -->
 
     <!-- impact backgroud -->
-      <div class="impact-section" style="background-image: url('img/impact-bg.png')">
-          <form class="im-form text-center" action="" method="post">
-            <button class="btn btn-lg btn-dark im-btn" type="submit" name="button">Stay Tuned!</button>
-            <input type="email" name="" value="" placeholder="Enter Your Email Address" class="im-input">
-          </form>
-      </div>
+    <div class="impact-section text-center" style="background-image: url('img/impact-bg.png'); margin-top: 70px;">
+      <form class="container stay-form" action="" method="post">
+        <div class="row">
+          <div class="col-3 text-right">
+            <button class="stay-button" type="submit" name="button">Stay Tuned!</button>
+          </div>
+          <div class="col-9 text-left">
+            <input type="email" name="" value="" placeholder="Enter Your Email Address" class="stay-input">
+          </div>
+        </div>
+      </form>
+    </div>
     <!-- impact backgroud -->
 
     <!-- footer section -->
     <div class="footer text-right">
       <div class="container">
-        <div class="row py-4">
+        <div class="row py-4 text-right">
           <div class="col-md-4">
             <img src="img/hult_white.png" style="width: 70px;" alt="">
           </div>
-          <div class="col-md-4">
+          <div class="col-md-4 text-right">
             <img src="img/unicorn.png" style="width: 200px;" alt="">
           </div>
-          <div class="col-md-4 links">
+          <div class="col-md-4 text-right links">
             <h4>Curated Connections</h4>
             <hr>
             <a href="#"> <img src="img/twitter.png" class="link-img" alt=""> </a>
